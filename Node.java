ae21f23c0fb6e9ab1e76b9f6a880f80741498563
vaikunthsridharan <vaikunthsridharan@gmail.com>
2016-03-23 15:07:29 -0400

    First commit, Shortest path algo

---
+++ Node.java
@@ -0,0 +1,23 @@
+package algorithm.Dijkstra;
+
+import java.util.Comparator;
+
+public class Node implements Comparator<Node> {
+	public int nextNode;
+	public int nodeCost;
+	
+	public Node() {
+		this.nextNode = 0;
+		this.nodeCost = 0;
+	}
+
+	public Node(int node, int cost) {
+		this.nextNode = node;
+		this.nodeCost = cost;
+	}
+
+	@Override
+	public int compare(Node node1, Node node2) {
+		return Integer.compare(node1.nodeCost, node2.nodeCost);
+	}
+}
